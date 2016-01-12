dbdb39e85e2b7124e632169a040531e6f3f87f29
HuangYao <huangyao@renhenet.com.cn>
2016-01-12 23:48:35 +0800

    解析数据父接口

---
+++ crawler-core/src/main/java/cn/ideath/crawler/analyze/Parser.java
@@ -0,0 +1,13 @@
+package cn.ideath.crawler.analyze;
+
+import cn.ideath.crawler.bean.SourceData;
+
+/**
+ * 解析父接口
+ * @author HuangYao
+ *
+ */
+public interface Parser {
+
+	void parse(SourceData src);
+}
---
+++ crawler-core/src/main/java/cn/ideath/crawler/bean/SourceData.java
@@ -0,0 +1,20 @@
+package cn.ideath.crawler.bean;
+
+/**
+ * 源数据
+ * @author HuangYao
+ *
+ */
+public class SourceData {
+
+	private String content; // 内容
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
