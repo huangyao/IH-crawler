d29925cbc3d03ca348aab3bfdf449bd10e9fb183
huangyao <116393990@qq.com>
2016-01-14 19:10:36 +0800

    可持久化的接口

---
+++ crawler-core/src/main/java/cn/ideath/crawler/persist/Persistent.java
@@ -0,0 +1,12 @@
+package cn.ideath.crawler.persist;
+/**
+ * Title: Persistent.java<br>
+ * Description: 可持久化的<br>
+ * Copyright (c) 人和网版权所有 2016    <br>
+ * Create DateTime: 2016年1月14日 下午4:36:41 <br>
+ * @author HuangYao
+ */
+public interface Persistent {
+	
+	void persist(Object obj);
+}
