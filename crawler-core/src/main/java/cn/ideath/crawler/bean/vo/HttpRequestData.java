67d752e8ce1f607ef4d893e47746cc47038553ac
huangyao <116393990@qq.com>
2016-01-15 14:14:10 +0800

    增加Http请求相关bean

--- crawler-core/src/main/java/cn/ideath/crawler/bean/vo/HttpRequestData.java
+++ crawler-core/src/main/java/cn/ideath/crawler/bean/vo/HttpRequestData.java
@@ -8,9 +8,11 @@ import cn.ideath.crawler.bean.RequestData;
  *
  */
 public class HttpRequestData implements RequestData {
-	
+
 	/** 请求地址 */
 	private String url;
+	/** 请求方法 */
+	private String method;
 
 	public String getUrl() {
 		return url;
@@ -19,5 +21,13 @@ public class HttpRequestData implements RequestData {
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
 }
---
+++ crawler-core/src/main/java/cn/ideath/crawler/bean/vo/HttpResponseData.java
@@ -0,0 +1,53 @@
+package cn.ideath.crawler.bean.vo;
+
+import org.apache.http.Header;
+
+import cn.ideath.crawler.bean.ResponseData;
+
+/**
+ * Title: HttpResponseData.java<br>
+ * Description: <br>
+ * Copyright (c) 人和网版权所有 2016    <br>
+ * Create DateTime: 2016年1月15日 上午10:51:55 <br>
+ * @author HuangYao
+ */
+public class HttpResponseData implements ResponseData {
+
+	private int statusCode;
+	private String mimeType;
+	private Header[] headers;
+	private String body;
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	public Header[] getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(Header[] headers) {
+		this.headers = headers;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
