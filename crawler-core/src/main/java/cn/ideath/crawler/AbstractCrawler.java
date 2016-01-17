bee87a82b4a1ce894dfa7f53929934cffd45d5e2
HuangYao <huangyao@renhenet.com.cn>
2016-01-17 11:36:42 +0800

    完善爬虫抽象类

--- crawler-core/src/main/java/cn/ideath/crawler/AbstractCrawler.java
+++ crawler-core/src/main/java/cn/ideath/crawler/AbstractCrawler.java
@@ -5,11 +5,9 @@ import cn.ideath.crawler.bean.ResponseData;
 import cn.ideath.crawler.fetch.Fetcher;
 
 /**
- * Title: AbstractCrawler.java<br>
- * Description: <br>
- * Copyright (c) 人和网版权所有 2016    <br>
- * Create DateTime: 2016年1月15日 下午2:16:39 <br>
+ * 
  * @author HuangYao
+ *
  */
 public abstract class AbstractCrawler<Req extends RequestData, Res extends ResponseData> implements Runnable {
 
@@ -23,9 +21,17 @@ public abstract class AbstractCrawler<Req extends RequestData, Res extends Respo
 	 */
 	protected abstract void afterProcess();
 
-	private Fetcher<Req, Res> fetcher;
-
+	/**
+	 * 生成请求信息
+	 */
 	protected abstract Req createRequest();
+
+	/**
+	 * 处理返回结果的方法，主要的执行业务逻辑都在当中写
+	 */
+	protected abstract void process(Res response);
+
+	private Fetcher<Req, Res> fetcher;
 
 	private boolean stop = false;
 
@@ -41,10 +47,8 @@ public abstract class AbstractCrawler<Req extends RequestData, Res extends Respo
 		try {
 			while (!stop) {
 				beforeProcess();
-
 				Res response = fetcher.fetch(createRequest());
-				// TODO
-
+				process(response);
 				afterProcess();
 			}
 		} catch (Exception e) {
